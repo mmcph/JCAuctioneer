ca92253d61c06472d5ea3bb753ca78768ca99702
Marlon McPherson <mmcpherson5@cnm.edu>
2018-07-23 15:06:56 -0600

    Disabled .htaccess for local testing. Completed navbar/BG.

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
 		<!--Google Fonts-->
-		<link href="https://fonts.googleapis.com/css?family=Anonymous+Pro" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
 
 		<!--FontAwesome-->
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
@@ -36,28 +36,29 @@
 		<!--favicon-->
 		<link rel="icon" href="">
 
-		<title>Jill Cook, Auctioneer</title>
+		<title>Jill Cook - Auctioneer</title>
 	</head>
 
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#top">Jill Cook, Auctioneer</a>
+		<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+			<a class="navbar-brand" href="#top"><i>Jill Cook - Auctioneer</i></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
 					  aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<ul class="navbar-nav mr-auto">
+				<ul class="navbar-nav ml-auto">
 					<li class="nav-item">
-						<a class="nav-link" href="#">About</a>
+						<a class="nav-link" href="#"><i>About</i></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Contact</a>
+						<a class="nav-link link2" href="#"><i>Contact</i></a>
 					</li>
 				</ul>
 			</div>
 		</nav>
+<div class="test"></div>
 	</body>
 
 </html>
